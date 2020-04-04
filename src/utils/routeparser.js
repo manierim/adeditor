b308e23c8758dfb3f5b4f4ae86dc55b0ec9e11d6
Marco <marco@italiatekno.it>
2020-04-04 23:56:16 +0200

    bugfix double bidirectional paths

--- src/utils/routeparser.js
+++ src/utils/routeparser.js
@@ -286,7 +286,7 @@ export default class routeParser {
           }
 
           if (linkType === null) {
-            BidirectionalInitialWaypointsDone.push(prevwpt);
+            BidirectionalInitialWaypointsDone.push(prevwpt.index);
           }
 
           path = {

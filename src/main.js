a0aede652297fb6967c3c48566cd0a988f4c0fa3
Marco <marco@italiatekno.it>
2020-04-20 08:13:23 +0200

    build(ui): :heavy_plus_sign: add v-tooltip

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './assets/tailwind.css'
+import Vue from "vue";
+
+import "./assets/tooltips.css";
+import VTooltip from "v-tooltip";
+Vue.use(VTooltip);
 
 require("typeface-roboto");
 require("material-icons");
+import "./assets/tailwind.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+import App from "./App.vue";
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
